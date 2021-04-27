6197556016ebd2418de8526234ab48dbc93787f1
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2021-04-27 10:30:21 -0300

    add animation for new messages

--- client/src/pages/Chat/ChatWindow/Message/styles.js
+++ client/src/pages/Chat/ChatWindow/Message/styles.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
+import { keyframes } from 'styled-components'
 
+const breatheAnimation = keyframes`
+ from {
+    opacity: 0;
+  }
+  to {
+    opacity: 0.9;
+  }
+`
 export const Container = styled.div`
  opacity: 0.9;
  margin: 15px 0;
@@ -8,7 +17,11 @@ export const Container = styled.div`
  border-radius: 5px;
  background: #ffffff10;
 
+ animation-name: ${breatheAnimation};
+ animation-duration: 1s;
+
  strong{
      font-size: 1.2rem;
+     border-bottom: solid 1px #ffffff50;
  }
 `
--- client/src/pages/Chat/ChatWindow/index.js
+++ client/src/pages/Chat/ChatWindow/index.js
@@ -6,7 +6,7 @@ import { Container } from './styles'
 const ChatWindow = (props) => {
     const chat = props.chat
         .map(m => <Message
-            key={Date.now() * Math.random()}
+            key={m.user + m.message + m.dataHora}
             user={m.user}
             message={m.message}
             dataHora={m.dataHora} />);
