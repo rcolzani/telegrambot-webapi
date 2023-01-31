7f20d3b7459ef9c01ffd6374998f1c0a44edf21e
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2023-01-31 09:32:36 -0300

    feat: change host url to railway.app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Routes from './routes';
-import Chat from './pages/Chat';
-import Dashboard from './pages/Dashboard'
 import GlobalStyle from './styles/global'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
